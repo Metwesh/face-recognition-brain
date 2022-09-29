618d7e53a92efe3a9245c185535a8f8139de2c11
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-09-29 14:44:14 +0200

    Fixed CORS

--- server.js
+++ server.js
@@ -23,34 +23,6 @@ const knex = require("knex")({
   },
 });
 
-const register = require("./controllers/register");
-const signin = require("./controllers/signin");
-const profile = require("./controllers/profile");
-const image = require("./controllers/image");
-const imageApi = require("./controllers/imageapi");
-const auth = require("./middleware/authorization");
-
-const app = express();
-
-// Development
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000"],
-//   })
-// );
-
-// Production
-app.use(
-  cors({
-    origin: ["https://face-detect-m-brain.herokuapp.com/"],
-  })
-);
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use(morgan("combined"));
-
 // Development
 {
   /* const redisClient = createClient({ url: process.env.REDIS_URI }); */
@@ -71,27 +43,67 @@ const redisClient = createClient({
   await redisClient.connect();
 })();
 
-app.get("/", (_req, res) => res.send("App is running"));
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+const imageApi = require("./controllers/imageapi");
+const auth = require("./middleware/authorization");
 
-app.post("/register", register.handleRegister(redisClient, knex, bcrypt));
+const app = express();
 
-app.post("/signin", signin.handleSigninAuth(redisClient, knex, bcrypt));
+// Development
+app.use(cors());
+
+// Production
+// app.use(
+//   cors({
+//     origin: ["https://face-detect-m-brain.herokuapp.com/"],
+//   })
+// );
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(morgan("combined"));
+
+app.get("/", cors(), (_req, res) => res.send("App is running"));
+
+app.post(
+  "/register",
+  cors(),
+  register.handleRegister(redisClient, knex, bcrypt)
+);
+
+app.post("/signin", cors(), signin.handleSigninAuth(redisClient, knex, bcrypt));
 
 app.get(
   "/profile/:id",
+  cors(),
   auth.requireAuth(redisClient),
   profile.handleProfileGet(knex)
 );
 
 app.put(
   "/profile/:id",
+  cors(),
   auth.requireAuth(redisClient),
   profile.handleProfileUpdate(knex, bcrypt)
 );
 
-app.put("/image", auth.requireAuth(redisClient), image.handleImage(knex));
+app.put(
+  "/image",
+  cors(),
+  auth.requireAuth(redisClient),
+  image.handleImage(knex)
+);
 
-app.post("/imageurl", auth.requireAuth(redisClient), imageApi.handleApiCall());
+app.post(
+  "/imageurl",
+  cors(),
+  auth.requireAuth(redisClient),
+  imageApi.handleApiCall()
+);
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(`App is running on port ${process.env.PORT || 3001}`);
